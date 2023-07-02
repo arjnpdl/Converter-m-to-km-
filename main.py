9fb758bfd7d8f78c1b8f2bc65974d4bc59ccd187
dEfy <64855738+arjnpdl@users.noreply.github.com>
2023-07-02 22:38:51 +0545

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+import tkinter as tk
+#from tkinter import ttk
+import ttkbootstrap as ttk
+
+def convert():
+	Float_input = entry_Strg.get()
+	meter_input = float(Float_input)
+	km_ouptut = meter_input / 1000
+	output_Strg.set(km_ouptut)
+
+
+#window
+win = ttk.Window(themename = 'darkly')
+win.title('Converter-dEfy')
+win.geometry('300x150')
+
+#title
+title_label = ttk.Label(master = win, text = 'Meters to Kilometers', font = 'Calibri 24 bold')
+title_label.pack()
+
+#input field
+input_frame = ttk.Frame(master = win)
+entry_Strg = tk.StringVar()
+entry = ttk.Entry(master = input_frame, textvariable = entry_Strg)
+button = ttk.Button(master = input_frame, text = 'Convert', command = convert)
+entry.pack(side = 'left',padx = 10)
+button.pack(side = 'left')
+input_frame.pack(pady = 10)
+
+#output
+output_Strg = tk.StringVar()
+output_label = ttk.Label(master = win, text = 'Output', font = 'Calibri 24', textvariable = output_Strg)
+output_label.pack()
+
+#run
+win.mainloop()
